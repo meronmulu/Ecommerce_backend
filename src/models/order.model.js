837db269c49dc619ac3541eb2d58d8912bef1eb7
yohannes-Ayenew <152222437+yohannes-Ayenew@users.noreply.github.com>
2026-02-02 23:55:01 +0300

    setup related to order

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -7,31 +7,36 @@ const orderSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
-
     sellerId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
-
     productId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Product",
       required: true,
     },
-
     amount: {
       type: Number,
       required: true,
     },
-
+    // UPDATED STATUS ENUMS FOR ESCROW
     status: {
       type: String,
-      enum: ["PENDING", "PAID", "CANCELLED"],
+      enum: [
+        "PENDING", // Order created, not paid
+        "ESCROW_HELD", // Paid, Money held by App
+        "SHIPPED", // Seller sent item
+        "DELIVERED", // Item reached buyer
+        "COMPLETED", // Buyer accepted, Money released
+        "DISPUTED", // Problem reported
+        "CANCELLED", // Cancelled before payment
+      ],
       default: "PENDING",
     },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 module.exports = mongoose.model("Order", orderSchema);
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -4,15 +4,27 @@ const {
   createOrder,
   getMyOrders,
   cancelOrder,
-  getAllOrders
+  getAllOrders,
+  completeOrder, // Import the new function
 } = require("../controller/order.controller");
-const {authenticateUser,authorizeRoles} = require("../middlewares/authMiddleware");
+const {
+  authenticateUser,
+  authorizeRoles,
+} = require("../middlewares/authMiddleware");
 
+// Create Order
 router.post("/", authenticateUser, createOrder);
+
+// Get My Orders
 router.get("/my", authenticateUser, getMyOrders);
+
+// Cancel Order
 router.patch("/:id/cancel", authenticateUser, cancelOrder);
-router.get("/all", authenticateUser,authorizeRoles("ADMIN"), getAllOrders);
 
+// Buyer Confirms Receipt (Releases Money)
+router.put("/:id/complete", authenticateUser, completeOrder);
 
+// Admin Get All
+router.get("/all", authenticateUser, authorizeRoles("ADMIN"), getAllOrders);
 
 module.exports = router;
